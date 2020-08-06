7182a43617f4ee8e609923ebc610e03a8681811b
Mariona <60394292+Marionaclto@users.noreply.github.com>
2020-08-06 12:33:08 +0200

    Add files via upload

---
+++ seeders/20200701093856-users.js
@@ -0,0 +1,31 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('users', [
+      {
+        name: 'Anna',
+        mail: 'atowarek@gmail.com',
+        phone: '654321111',
+        password: bcrypt.hashSync('atowarek', 10),
+      },
+      {
+        name: 'Esther',
+        mail: 'estherurzanqui81@gmail.com',
+        phone: '654321987',
+        password: bcrypt.hashSync('eurzanqui', 10),
+      },
+      {
+        name: 'Mariona',
+        mail: 'mariona.clto@gmail.com',
+        phone: '654321986',
+        password: bcrypt.hashSync('mclara', 10),
+      },
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('user', null, {})
+  },
+}
